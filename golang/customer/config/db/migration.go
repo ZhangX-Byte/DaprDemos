eadbc5562c160bed5d5ca1494f344a935100300b
liuhongxi <liuhongxi0000@163.com>
2019-12-25 09:51:40 +0800

    auto migrate

--- golang/customer/config/db/migration.go
+++ golang/customer/config/db/migration.go
@@ -3,12 +3,5 @@ package db
 import "daprdemos/golang/customer/models"
 
 func init() {
-	AutoMigrate(&models.Customer{})
-}
-
-// AutoMigrate run auto migration
-func AutoMigrate(values ...interface{}) {
-	for _, value := range values {
-		DB.AutoMigrate(value)
-	}
+	DB.AutoMigrate(&models.Customer{})
 }
